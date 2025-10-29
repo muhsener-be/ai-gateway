a3854922cdb749e7dcac4447bc8b18c1596d70e3
muhsener-be <muhammet.sener@bilgiedu.net>
2025-10-30 01:23:38 +0300

    Bismillahirrahmanirrahim

---
+++ src/test/java/com/github/muhsenerbe/aigateway/AiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.muhsenerbe.aigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
